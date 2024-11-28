098b938ca6d6784018cbe002b714a28660b28d08
XaviVeC <xavivegacuadros@gmail.com>
2024-11-28 18:49:49 +0100

    JavaDoc del constructor Alumne
    Co-authored-by: Carlaemo <Carlaemo@users.noreply.github.com>
    Co-authored-by: DaniMolina11 <DaniMolina11@users.noreply.github.com>

--- src/main/java/prac3/integrants/Alumne.java
+++ src/main/java/prac3/integrants/Alumne.java
@@ -6,6 +6,21 @@ public class Alumne extends Membre {
     private int anysEtse;
     private boolean graduat; // Per saber si l'alumne s'ha graduat ja o no 
 
+    /**
+     * Constructor de la classe Alumne.
+     * @param nom - nom de l'alumne
+     * @param alias - alias de l'alumne
+     * @param correu - correu de l'alumne
+     * @param diaAlta - dia que es va donar de baixa l'alumne
+     * @param mesALta - mes que es va donar d'alta l'alumne
+     * @param anyAlta - any que es va donar d'alta l'alumne
+     * @param diaBaixa - dia que es va donar de baixa l'alumne
+     * @param mesBaixa - mes en que es dona de baixa l'alumne
+     * @param anyBaixa - any en que es dona de baixa l'alumne 
+     * @param curs - curs de l'alumne
+     * @param anysEtse - anys cursats en la ETSE
+     * @return - Objecte de la classe Alumne.
+     */
     public Alumne (String nom, String alias, String correu, int diaAlta, int mesALta, int anyAlta, int diaBaixa, int mesBaixa, int anyBaixa, String curs, int anysEtse){
         super(nom, alias, correu, diaAlta, mesALta, anyAlta, diaBaixa, mesBaixa, anyBaixa);
         this.graduat = false;
@@ -13,6 +28,9 @@ public class Alumne extends Membre {
         this.anysEtse = anysEtse;
     }
 
+    /**
+     * 
+     */
     public String toString (){
         return("Nom:" + getNom()+ "\n" +
                "Alias:" + getAlias() +  "\n" +
