d62a8fb1c7684cd52897d79e123bfa3570d32c98
“Daniel <daniel.molina@urv.cat>
2024-12-02 11:41:53 +0100

    Comentarios de la clase Alumne en javadoc, ya se han añadido todas las capçaleras

--- src/main/java/prac3/integrants/Alumne.java
+++ src/main/java/prac3/integrants/Alumne.java
@@ -4,22 +4,22 @@ public class Alumne extends Membre {
 
     private String curs;
     private int anysEtse;
-    private boolean graduat; // Per saber si l'alumne s'ha graduat ja o no 
+    private boolean graduat; // Variable que indica si el alumno se ha graduado ya o no 
 
     /**
-     * Constructor de la classe Alumne.
-     * @param nom - nom de l'alumne
-     * @param alias - alias de l'alumne
-     * @param correu - correu de l'alumne
-     * @param diaAlta - dia que es va donar de baixa l'alumne
-     * @param mesALta - mes que es va donar d'alta l'alumne
-     * @param anyAlta - any que es va donar d'alta l'alumne
-     * @param diaBaixa - dia que es va donar de baixa l'alumne
-     * @param mesBaixa - mes en que es dona de baixa l'alumne
-     * @param anyBaixa - any en que es dona de baixa l'alumne 
-     * @param curs - curs de l'alumne
-     * @param anysEtse - anys cursats en la ETSE
-     * @return - Objecte de la classe Alumne.
+     * Constructor de la clase Alumne.
+     * @param nom - nombre del alumno
+     * @param alias - alias del alumno
+     * @param correu - correo del alumno
+     * @param diaAlta - dia que se dio de baja el alumno
+     * @param mesALta - mes que se dio de alta el alumno
+     * @param anyAlta - año que se dio de alta el alumno
+     * @param diaBaixa - dia que se dio de baja el alumno
+     * @param mesBaixa - mes que se dio de baja el alumno
+     * @param anyBaixa - año que se dio de baja el alumno 
+     * @param curs - curso del alumno
+     * @param anysEtse - anyos cursados en la ETSE
+     * @return - Objecto de la clase Alumne.
      */
     public Alumne (String nom, String alias, String correu, int diaAlta, int mesALta, int anyAlta, int diaBaixa, int mesBaixa, int anyBaixa, String curs, int anysEtse){
         super(nom, alias, correu, diaAlta, mesALta, anyAlta, diaBaixa, mesBaixa, anyBaixa);
@@ -29,7 +29,7 @@ public class Alumne extends Membre {
     }
 
     /**
-     * 
+     * Metodo String que imprime los datos de la clase alumno
      */
     public String toString (){
         return("Nom:" + getNom()+ "\n" +
@@ -42,22 +42,41 @@ public class Alumne extends Membre {
                "Graduat:" + getGraduat());
     }
     
+    /**
+     * Getter de la variable curs
+     * @return - variable curs
+     */
     public String getCurs (){
         return curs;
     }
 
+    /**
+     * Getter de la variable anyEtse
+     * @return - variable anysEtse
+     */
     public int getAnysEtse (){
         return anysEtse;
     }
 
+    /**
+     * Modificador de la variable graduat
+     */
     public void JaGraduat(){
         graduat = true;
     }
 
+    /**
+     * Getter de la variable graduat
+     * @return - variable graduat
+     */
     public boolean getGraduat(){
         return graduat;
     }
 
+    /**
+     * Metodo que hace una copia de un alumno en concreto
+     * @return - copia de alumno en concreto
+     */
     public Alumne copiaAlumne(){
         Alumne a = new Alumne(getNom(),getAlias(), getCorreu(), getDiaAlta(),getMesAlta(), getAnyAlta(), getDiaBaixa(), getMesBaixa(), getAnyBaixa(), curs, anysEtse);
         return a;
