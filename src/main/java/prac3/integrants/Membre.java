3a86812acdbd40cb00dc2ae4536dc70a19605b73
“Daniel <daniel.molina@urv.cat>
2024-12-02 15:24:10 +0100

    Comentarios en JavaDoc de las clases Professor y Membre

--- src/main/java/prac3/integrants/Membre.java
+++ src/main/java/prac3/integrants/Membre.java
@@ -4,13 +4,27 @@ package prac3.integrants;
 public abstract class Membre {
 
     //DADES
-    private String nom; // Nom del membre 
+    private String nom; // Nombre del miembro 
     private String alias;// Alias del membre 
-    private String correu;//Correu electrònic del membre 
-    private int diaAlta, mesALta, anyAlta; // Data en la que es dona de alta 
-    private int diaBaixa, mesBaixa, anyBaixa; // Data en la que es dona de baixa 
+    private String correu;// Correo electronico del miembro 
+    private int diaAlta, mesALta, anyAlta; // Fecha en la que se da de alta 
+    private int diaBaixa, mesBaixa, anyBaixa; // Fecha en la que se da de baja 
 
-    //MÉTODES 
+    //MÉTODES
+    
+    /**
+     * Metodo constructor de la clase Membre
+     * @param nom - Nombre del miembro
+     * @param alias - Alias del nombre del miembro
+     * @param correu - Correo del miembro
+     * @param diaAlta - Dia en el que se dio de alta
+     * @param mesALta - Mes en el que se dio de alta
+     * @param anyAlta - Año en el que se dio de alta
+     * @param diaBaixa - Dia en el que se dio de baja
+     * @param mesBaixa - Mes en el que se dio de baja
+     * @param anyBaixa - Año en el que se dio de baja
+     * @return - Constructor Membre
+     */
     public Membre (String nom, String alias, String correu, int diaAlta, int mesALta, int anyAlta, int diaBaixa, int mesBaixa, int anyBaixa){
         this.nom = nom;
         this.alias = alias;
@@ -24,50 +38,100 @@ public abstract class Membre {
 
     }
 
+    /**
+     * Getter de la variable nom
+     * @return - variable nom
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Getter de la variable alias
+     * @return - variable alias
+     */
     public String getAlias(){
         return alias;
     }
 
+    /**
+     * Getter de la variable correu
+     * @return - variable correu
+     */
     public String getCorreu(){
         return correu;
     }
 
+    /**
+     * Getter de la fecha exacta en la que se dio de alta
+     * @return - dia, mes y año en el que se dio de alta
+     */
     public String getDataAlta(){
         return diaAlta+ "/" +mesALta+ "/" +anyAlta;
     }
 
+    /**
+     * Getter de la fecha exacta en la que se dio de baja
+     * @return - dia, mes y año en el que se dio de baja
+     */
     public String getDataBaixa(){
         return diaBaixa+ "/" +mesBaixa+ "/" +anyBaixa;
     }
 
+    /**
+     * Getter del dia que se dio de alta
+     * @return - variable diaAlta
+     */
     public int getDiaAlta(){
         return diaAlta;
     }
 
+    /**
+     * Getter del mes en el que se dio de alta
+     * @return - variable mesAlta
+     */
     public int getMesAlta(){
         return mesALta;
     }
 
+    /**
+     * Getter del año en el que se dio de alta
+     * @return - variable anyAlta
+     */
     public int getAnyAlta(){
         return anyAlta;
     }
 
+    /**
+     * Getter del dia que se dio de baja
+     * @return - variable diaBaixa
+     */
     public int getDiaBaixa(){
         return diaBaixa;
     }
 
+    /**
+     * Getter del mes en el que se dio de baja
+     * @return - variable mesBaixa
+     */
     public int getMesBaixa(){
         return mesBaixa;
     }
 
+    /**
+     * Getter del año en el que se dio de baja
+     * @return - variable anyBaixa
+     */
     public int getAnyBaixa(){
         return anyBaixa;
     }
 
+    /**
+     * Metodo que da de alta a un miembro, lo unico que hace es guardar la fecha que se le pasa en los parametros del alta
+     * @param diaAlta - dia en el que se quiere dar de alta
+     * @param mesALta - mes en el que se quiere dar de alta
+     * @param anyAlta - año en el que se quiere dar de alta
+     */
     public void DonarDeAlta(int diaAlta, int mesALta, int anyAlta){
         //Comprobar que el membre no esta ja donat de alta
         if (this.diaAlta == 0 && this.mesALta == 0 && this.anyAlta == 0){
@@ -77,6 +141,12 @@ public abstract class Membre {
         }
     }
 
+    /**
+     * Metodo que da de baja a un miembro, lo unico que hace es guardar la fecha que se le pasa en los parametros de la baja
+     * @param diaBaixa - dia en que se quiere dar de baja
+     * @param mesBaixa - mes en que se quiere dar de baja
+     * @param anyBaixa - año en que se quiere dar de baja
+     */
     public void DonarDeBaixa(int diaBaixa, int mesBaixa, int anyBaixa){
         //Comprobar que el membre esta donat de Alta 
         if (this.diaAlta != 0 && this.mesALta != 0 && this.anyAlta != 0){
--- src/main/java/prac3/integrants/Professor.java
+++ src/main/java/prac3/integrants/Professor.java
@@ -2,9 +2,25 @@ package prac3.integrants;
 
 public class Professor extends Membre {
 
-    private String departament;
-    private int numeroDespatx; 
+    private String departament; // si es DEIM o DEEEA
+    private int numeroDespatx; // numero de su despacho
 
+
+    /**
+     * Metodo Constructor de la clase Professor
+     * @param nom - Nombre del profesor
+     * @param alies - Alias del profesor
+     * @param correu - Correo del profesor
+     * @param diaAlta - Dia en que se dio de alta el profesor
+     * @param mesALta - Mes en que se dio de alta el profesor
+     * @param anyAlta - Año en que se dio de alta el profesor
+     * @param diaBaixa - Dia en que se dio de baja el profesor
+     * @param mesBaixa - Mes en que se dio de baja el profesor
+     * @param anyBaixa - Año en que se dio de baja el profesor
+     * @param departament - Si es DEIM o DEEEA
+     * @param numeroDespatx - Numero de despacho del profesor
+     * @return - Constructor de profesor
+     */
     public Professor (String nom, String alies, String correu, int diaAlta, int mesALta, int anyAlta, int diaBaixa, int mesBaixa, int anyBaixa,String departament, int numeroDespatx){
         super(nom, alies, correu, diaAlta, mesALta, anyAlta, diaBaixa, mesBaixa, anyBaixa);
         this.departament = departament;
@@ -12,14 +28,25 @@ public class Professor extends Membre {
       
     }
 
+    /**
+     * Getter de la variable departament
+     * @return - variable departament
+     */
     public String getDepartament() {
         return departament;
     }
 
+    /**
+     * Getter de la variable numeroDespatx
+     * @return - variable numeroDespatx
+     */
     public int getNumeroDespatx (){
         return numeroDespatx;
     }
    
+    /**
+     * Metodo String que imprime todos los datos del objeto profesor
+     */
     public String toString(){
         return ("Nom:" + getNom()+ "\n" +
                "Alias:" + getAlias() +  "\n" +
@@ -30,6 +57,10 @@ public class Professor extends Membre {
                "Numero del despatx:" +getNumeroDespatx());
     }
 
+    /**
+     * Metodo que crea una copia de un profesor
+     * @return - copia de un profesor
+     */
     public Professor copiaProfessor(){
         Professor p = new Professor(getNom(), getAlias(),getCorreu(), getDiaAlta(), getMesAlta(), getAnyAlta(), getDiaBaixa(), getMesBaixa(), getAnyBaixa(),departament,numeroDespatx);
         return p;
