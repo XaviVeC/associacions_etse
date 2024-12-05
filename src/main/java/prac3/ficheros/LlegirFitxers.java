b71c66590bd0a3bee3ed08d2ccb8d87955c99599
Carlaemo <carlaemomoreno@gmail.com>
2024-12-05 18:50:25 +0100

    lectura ficheros Miembros y Asociaciones acabado

--- src/main/java/prac3/ficheros/LlegirFitxers.java
+++ src/main/java/prac3/ficheros/LlegirFitxers.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import prac3.associacions.Associacio;
+import prac3.estructures.Data;
 import prac3.estructures.LlistaAccions;
 import prac3.estructures.LlistaAssociacions;
 import prac3.estructures.LlistaMembres;
@@ -20,13 +22,12 @@ public class LlegirFitxers {
         try(BufferedReader lectura = new BufferedReader(new FileReader (nombreFichero))){
             
             String linia = "";
-            int indiceFichero, numeroDespacho, yearsEtse;
+            int indiceFichero, numeroDespacho, yearsEtse, diaAlta, mesAlta, yearAlta, diaBaja, mesBaja, yearBaja;
             String tipoMiembro, nombreMiembro, alias, correoMiembro, departamento, curso;
             String[] trozos, trozosFechaAlta, trozosFechaBaja;
             boolean graduado;
-            Data fechaAlta;
-            Data fechaBaja;
-            
+            Data fechaAlta, fechaBaja;
+
             linia = lectura.readLine();
             while (linia != null) {
 
@@ -36,34 +37,33 @@ public class LlegirFitxers {
                 nombreMiembro = trozos [2];
                 alias = trozos [3];
                 correoMiembro = trozos[4];
+
+                 
                 trozosFechaAlta = trozos[5].split("-");
+                diaAlta = Integer.parseInt(trozosFechaAlta[0]);
+                mesAlta = Integer.parseInt(trozosFechaAlta[1]);
+                yearAlta = Integer.parseInt(trozosFechaAlta[2]);
+                fechaAlta = new Data(diaAlta,mesAlta,yearAlta);
+
                 trozosFechaBaja = trozos[6].split("-");
+                diaBaja = Integer.parseInt(trozosFechaBaja[0]);
+                mesBaja = Integer.parseInt(trozosFechaBaja[1]);
+                yearBaja = Integer.parseInt(trozosFechaBaja[2]);
+                fechaBaja = new Data (diaBaja,mesBaja,yearBaja);
                 
-                
-                /*
-                 * 
-                
-                StringTokenizer st = new StringTokenizer(linia,";");
-                indiceFichero = Integer.parseInt(st.nextToken());
-                tipoMiembro = st.nextToken();
-                nombreMiembro = st.nextToken();
-                alias = st.nextToken();
-                correoMiembro = st.nextToken();
-                //fechaAlta = - falta mirar como leerlo 
-                //fechaBaja = -falta mirar como leerlo 
-                */
+
 
                 switch(tipoMiembro){
                     case "Alumno":
-                        curso = st.nextToken();
-                        yearsEtse = Integer.parseInt(st.nextToken());
-                        graduado = st.hasMoreTokens();
-                        Alumne a = new Alumne(indiceFichero, tipoMiembro, nombreMiembro, alias, correoMiembro,fechaAlta,fechaBaja, curso, yearsEtse, graduado);
+                        curso = trozos[7];
+                        yearsEtse = Integer.parseInt(trozos[8]);
+                        graduado = Boolean.parseBoolean(trozos[9]);
+                        Alumne a = new Alumne(indiceFichero, tipoMiembro, nombreMiembro, alias, correoMiembro, fechaAlta, fechaBaja, curso, yearsEtse, graduado);
                         listaM.addmiembro(a);
                         break;
                     case "Profesor":
-                        departamento = st.nextToken();
-                        numeroDespacho = Integer.parseInt(st.nextToken());
+                        departamento = trozos[7];
+                        numeroDespacho = Integer.parseInt(trozos[8]);
                         Professor p = new Professor(indiceFichero, tipoMiembro, nombreMiembro, alias, correoMiembro, fechaAlta, fechaBaja, departamento, numeroDespacho);
                         listaM.addmiembro(p);
                         break;
@@ -82,34 +82,39 @@ public class LlegirFitxers {
         try(BufferedReader lectura = new BufferedReader(new FileReader(nombreFichero))){
             String linia = "";
             String nombreAsociacion, correoContactoAsociacion;
-            String[] titulacionesAsociacion;
+            String[] titulacionesAsociacion, trozos;
             Membre[] listaMiembrosAsociacion, personasEnCargos;
 
             linia = lectura.readLine();
             while (linia != null){
-                StringTokenizer st = new StringTokenizer(linia,";");
-                StringTokenizer st2 = new StringTokenizer(linia,"/");
-                nombreAsociacion = st.nextToken();
-                correoContactoAsociacion = st.nextToken();
-                //titulacionesAsociacion = falta mirar como leerlo 
-                //listaMiembrosAsociacion = falta mirar como leerlo 
-                //personasEnCargos = falta mirar como leerlo 
-            
-            Membre miembro = listaM.miembrosDeAsociacionConcreta(nombreAsociacion);
+                
+                trozos = linia.split(";");
+                nombreAsociacion = trozos[0];
+                correoContactoAsociacion = trozos[1];
+                titulacionesAsociacion = trozos[2].split("/");// Supongo que el contenido de dentro de la lista de Strings esta separado por una barra 
+                String[] nombresDeLosMiembros = trozos[3].split(",");
+                listaMiembrosAsociacion = new Membre[nombresDeLosMiembros.length];
+                String [] nombresCargos = trozos[4].split("/");
+                personasEnCargos = new Membre[nombresCargos.length]; 
+                
+            LlistaMembres miembro = listaM.miembrosDeAsociacionConcreta(nombreAsociacion); //!!!!! NO SE SI ESTA BIEN 
 
             if(miembro != null){
-                
 
+                Associacio associacio = new Associacio(nombreAsociacion, correoContactoAsociacion, titulacionesAsociacion, listaMiembrosAsociacion, personasEnCargos);
+                listaAso.addAsociacion(associacio);
             }
-                
-
-
+            
             }
-
-
-        }
-
+            lectura.close();
+            }catch(FileNotFoundException e){
+                System.out.println("el fichero actividades no se ha encontrado" + e.toString());
+            }catch(IOException ex){
+               System.err.println("Error en la lectura " + ex.toString());
+            }
     }
+
+
 
     
 
