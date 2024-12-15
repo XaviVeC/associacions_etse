529ed2a0e984529b724612222f8f125101e2da98
Carlaemo <carlaemomoreno@gmail.com>
2024-12-15 18:34:55 +0100

    Comienzo main grafico

--- src/main/java/prac3/Main_Grafico/Main_Grafico.java
+++ src/main/java/prac3/Main_Grafico/Main_Grafico.java
@@ -1,5 +1,69 @@
 package prac3.Main_Grafico;
 
-public class Main_Grafico {
 
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import prac3.Estructuras.ListaAcciones;
+
+public class Main_Grafico  extends JFrame implements ActionListener{
+
+    private static String[] acciones = {"Charla", "Demostracion"};
+   private JPanel panelBotones;
+   private JButton[] botonesAcciones;
+   
+
+   public Main_Grafico(String titulo, ListaAcciones listaTodasLasAcciones){
+        super (titulo);
+    
+        this.setSize(400, 300);
+        this.setLocation(570, 220);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contenedor = getContentPane();
+        contenedor.setLayout(new BorderLayout());
+
+        panelBotones = new JPanel();
+        panelBotones.setLayout(new FlowLayout());
+        botonesAcciones = new JButton[acciones.length];
+
+        for (int i = 0; i< acciones.length; i++){
+            botonesAcciones[i] = new JButton(acciones[i]);
+            botonesAcciones[i].setBackground(Color.white);
+            botonesAcciones[i].addActionListener(this);
+            panelBotones.add(botonesAcciones[i]);    
+        }
+        contenedor.add(panelBotones, BorderLayout.NORTH);
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800,400);
+        this.setVisible(true);
+
+
+       
+
+
+    }
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
 }
+}
+
+
+
+   
+
